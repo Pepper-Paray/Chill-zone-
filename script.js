1448d952d1ce036acce699816c0ff29ff73f9944
Pepper Paray <pandapup3000@gmail.com>
2025-07-10 15:19:34 -0500

    corrected some js and add some to the chat

--- script.js
+++ script.js
@@ -18,90 +18,15 @@ import {
   ShieldCheckIcon,
   UserIcon,
 } from '@heroicons/react/16/solid'
-import { InboxIcon, MagnifyingGlassIcon } from '@heroicons/react/20/solid'
-function Example() {
-  
-    return (<Navbar>
-      <Dropdown>
-        <DropdownButton as={NavbarItem}>
-          <Avatar src="/tailwind-logo.svg" />
-          <NavbarLabel>Tailwind Labs</NavbarLabel>
-          <ChevronDownIcon />
-        </DropdownButton>
-        <DropdownMenu className="min-w-64" anchor="bottom start">
-          <DropdownItem href="/teams/1/settings">
-            <Cog8ToothIcon />
-            <DropdownLabel>Settings</DropdownLabel>
-          </DropdownItem>
-          <DropdownDivider />
-          <DropdownItem href="/teams/1">
-            <Avatar slot="icon" src="/tailwind-logo.svg" />
-            <DropdownLabel>Tailwind Labs</DropdownLabel>
-          </DropdownItem>
-          <DropdownItem href="/teams/2">
-            <Avatar slot="icon" initials="WC" className="bg-purple-500 text-white" />
-            <DropdownLabel>Workcation</DropdownLabel>
-          </DropdownItem>
-          <DropdownDivider />
-          <DropdownItem href="/teams/create">
-            <PlusIcon />
-            <DropdownLabel>New team&hellip;</DropdownLabel>
-          </DropdownItem>
-        </DropdownMenu>
-      </Dropdown>
-      <NavbarDivider className="max-lg:hidden" />
-      <NavbarSection className="max-lg:hidden">
-        <NavbarItem href="index.html" current>
-          Home
-        </NavbarItem>
-        <NavbarItem href="/chat.html">chat</NavbarItem>
-        <NavbarItem href="/therapy.html">therapy resouces</NavbarItem>
-      </NavbarSection>
-      <NavbarSpacer />
-      <NavbarSection>
-        <NavbarItem href="/search" aria-label="Search">
-          <MagnifyingGlassIcon />
-        </NavbarItem>
-        <NavbarItem href="/inbox" aria-label="Inbox">
-          <InboxIcon />
-        </NavbarItem>
-        <Dropdown>
-          <DropdownButton as={NavbarItem}>
-            <Avatar src="/profile-photo.jpg" square />
-          </DropdownButton>
-          <DropdownMenu className="min-w-64" anchor="bottom end">
-            <DropdownItem href="/my-profile">
-              <UserIcon />
-              <DropdownLabel>My profile</DropdownLabel>
-            </DropdownItem>
-            <DropdownItem href="/settings">
-              <Cog8ToothIcon />
-              <DropdownLabel>Settings</DropdownLabel>
-            </DropdownItem>
-            <DropdownDivider />
-            <DropdownItem href="/privacy-policy">
-              <ShieldCheckIcon />
-              <DropdownLabel>Privacy policy</DropdownLabel>
-            </DropdownItem>
-            <DropdownItem href="/share-feedback">
-              <LightBulbIcon />
-              <DropdownLabel>Share feedback</DropdownLabel>
-            </DropdownItem>
-            <DropdownDivider />
-            <DropdownItem href="/logout">
-              <ArrowRightStartOnRectangleIcon />
-              <DropdownLabel>Sign out</DropdownLabel>
-            </DropdownItem>
-          </DropdownMenu>
-        </Dropdown>
-      </NavbarSection>
-    </Navbar>
-  )
-}
-function openForm() {
-  document.getElementById("myForm").style.display = "block";
-}
+    
+<form id="chatForm">
+    <input type="text" id="message" placeholder="Type your message"></input>
+    <button type="submit">Send</button>
+</form>
 
-function closeForm() {
-  document.getElementById("myForm").style.display = "none";
-}
+    document.getElementById('chatForm').addEventListener('submit', function(event) {
+        event.preventDefault(); 
+        const message = document.getElementById('message').value;
+        console.log('Message sent:', message); 
+    });
+
