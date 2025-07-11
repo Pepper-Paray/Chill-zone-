65f406901d5f1b7a637b1347638df4918b8c71b5
Pepper Paray <pandapup3000@gmail.com>
2025-07-10 18:23:34 -0700

    api is down externally but is visible on screen

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
-function getQuotes() {
-  fetch("https://api.quotable.oi/random")
-   .then(Response => Response.json())
-   .then(Response => {
-    console.log(data);
-    document.getElementById("quote").textContent = `"${data.content}"`;
-    document.getElementById("author").textContent = `-${data.author}`;
-   })
+async function getRandomQuote() {
+  try {
+    const response = await fetch("https://api.quotable.io/random");
+    const data = await response.json();
+    document.getElementById('quote').textContent = `"${data.content}"`;
+    document.getElementById('author').textContent = `— ${data.author}`;
+  } catch (error) {
+    document.getElementById('quote').textContent = 'Could not fetch quote.';
+    document.getElementById('author').textContent = '';
+    console.error(error);
+  }
 }
-getQuotes();
+
+
+getRandomQuote();
 
